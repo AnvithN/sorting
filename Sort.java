d863009085a6c7f386a5870be48fa99dacfc40bc
AnvithN <1116879@apps.nsd.org>
2025-04-18 20:01:14 +0000

    Done

--- Sort.java
+++ Sort.java
@@ -29,10 +29,42 @@ public class Sort
       } while(swap);
     }
 
-    public static void mergesort(int[] array3)
-    {
-      int[] arr1 = new int[array3.length/2];
-    }
+    public static void mergesort(int[] array3) {
+      if (array3.length < 2) {
+          return; 
+      }
+  
+      int mid = array3.length / 2;
+      int[] left = new int[mid];
+      int[] right = new int[array3.length - mid];
 
-    
+      for (int i = 0; i < mid; i++) {
+          left[i] = array3[i];
+      }
+      for (int i = mid; i < array3.length; i++) {
+          right[i - mid] = array3[i];
+      }
+     
+      mergesort(left);
+      mergesort(right);
+      merge(array3, left, right);
+  }
+  private static void merge(int[] array3, int[] left, int[] right) {
+      int i = 0, j = 0, k = 0;
+  
+      while (i < left.length && j < right.length) {
+          if (left[i] <= right[j]) {
+              array3[k++] = left[i++];
+          } else {
+              array3[k++] = right[j++];
+          }
+      }
+      while (i < left.length) {
+          array3[k++] = left[i++];
+      }
+  
+      while (j < right.length) {
+          array3[k++] = right[j++];
+      }
+  }
   }
